81afb3adc962a81b976e5acb9966cbe2e5e51907
umarbrowser <umarbrowser20@gmail.com>
2022-07-10 21:47:43 +0100

    aseert

--- test_main.py
+++ test_main.py
@@ -29,6 +29,7 @@ def test_health(client):
 
 
 def test_auth(client):
+    
     body = {'email': EMAIL,
             'password': PASSWORD}
     response = client.post('/auth', 
@@ -38,3 +39,4 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+    assert False
